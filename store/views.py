e16ffd64dc7ea020401ad804feac1b59dffdc754
Suyash Chilwarwar <suyash.c@digitalleaf.io>
2023-01-08 17:05:16 +0530

    Completed till search functionality

--- store/views.py
+++ store/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.http import HttpResponse
 from cart.models import CartItem
 from cart.views import _cart_id
 from .models import Product
 from category.models import Category
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 
 
 # Create your views here.
@@ -43,4 +44,16 @@ def product_detail(request, category_slug, product_slug):
 
 
 def search(request):
-    pass
+    products = ''
+    product_count = 0
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
